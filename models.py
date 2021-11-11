16aff38ef1078195a740d4d69524a4b8e4c434a6
thanuavi1 <thanuavi1@gmail.com>
2021-11-11 22:38:21 +0000

    Login and Sign Up Pages, Created routes and basic flow, database set up

---
+++ models.py
@@ -0,0 +1,16 @@
+from app import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def get_username(self):
+        return self.email
+
+
+db.create_all()
