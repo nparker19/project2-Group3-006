50e2d80e42e87572d58c0cb7e37229339c98acbf
Nora Parker <nparker19@student.gsu.edu>
2021-11-14 15:30:04 -0500

    Added start and end time feature for events. Adjusted all associated routes and functions to match

--- methods.py
+++ methods.py
@@ -1,47 +1,65 @@
 from datetime import datetime, timedelta
 
-# A function which takes in a list of times (as a string, IN chronological order), and outputs schedule suggestions
+# A function which takes in a list of times (as a string), and outputs schedule suggestions
 def suggest(scheduleDict):
-    timeList = []
+    timeStartList = []
+    timeEndList = []
     eventList = []
     suggestList = []
 
+    # Schedule must be sorted in chronological order or the times will not be accurate
+    if len(scheduleDict) != 0:
+        scheduleDict = sorted(
+            scheduleDict, key=lambda x: datetime.strptime(x["startTime"], "%H:%M")
+        )
     for item in scheduleDict:
-        task_time = datetime.strptime(item["time"], "%I:%M %p")
-        timeList.append(task_time)
+        task_start_time = datetime.strptime(item["startTime"], "%H:%M")
+        task_end_time = datetime.strptime(item["endTime"], "%H:%M")
+        timeStartList.append(task_start_time)
+        timeEndList.append(task_end_time)
         eventList.append(item["event"])
 
-    if len(timeList) != 0:
-        initialTime = timeList[0]
-        for i in range(1, len(timeList)):
-            time_difference = timeList[i] - initialTime
-            if time_difference >= timedelta(hours=10):
-                suggestList.append(
-                    "You have a large amount of time between "
-                    + eventList[i - 1]
-                    + " and "
-                    + eventList[i]
-                    + ". Plenty of time to work out, or run errands"
-                )
-            if time_difference >= timedelta(hours=6) and time_difference < timedelta(
-                hours=10
-            ):
-                suggestList.append(
-                    "You have a good amount of time between "
-                    + eventList[i - 1]
-                    + " and "
-                    + eventList[i]
-                    + " You could get in a workout and a study session. "
-                )
-            if time_difference >= timedelta(hours=3) and time_difference < timedelta(
-                hours=6
-            ):
-                suggestList.append(
-                    "You have some time between "
-                    + eventList[i - 1]
-                    + " and "
-                    + eventList[i]
-                    + ". This would be a great time to study or get in quick nap."
-                )
-            initialTime = timeList[i]
+    initialTime = timeEndList[0]
+    for i in range(1, len(timeStartList)):
+        time_difference = timeStartList[i] - initialTime
+        if time_difference >= timedelta(hours=10):
+            suggestList.append(
+                "You have a over 9 hours of free time between "
+                + eventList[i - 1]
+                + " and "
+                + eventList[i]
+                + ". Plenty of time to work out, or run errands"
+            )
+        if time_difference >= timedelta(hours=6) and time_difference < timedelta(
+            hours=10
+        ):
+            suggestList.append(
+                "You have over 5 hours of free time between "
+                + eventList[i - 1]
+                + " and "
+                + eventList[i]
+                + ". You could get in a workout and a study session!"
+            )
+        if time_difference >= timedelta(hours=3) and time_difference < timedelta(
+            hours=6
+        ):
+            suggestList.append(
+                "You have a few hours of free time between "
+                + eventList[i - 1]
+                + " and "
+                + eventList[i]
+                + ". This would be a great time to study or get in quick nap."
+            )
+        initialTime = timeEndList[i]
     return suggestList
+
+
+if __name__ == "__main__":
+    ans = suggest(
+        scheduleDict=[
+            {"event": "class", "startTime": "09:00 AM", "endTime": "10:45 AM"},
+            {"event": "meeting", "startTime": "06:30 PM", "endTime": "07:00 PM"},
+            {"event": "meeting2", "startTime": "10:00 PM", "endTime": "11:15 PM"},
+        ]
+    )
+    print(ans)
