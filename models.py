ac0615710ca2d57e98fdf0d1a07ebe5deb88710f
thanuavi1 <thanuavi1@gmail.com>
2021-11-15 20:14:04 +0000

    Adding styling, and table to authenticate user in database

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80))
+    username = db.Column(db.String(80))
 
     def __repr__(self):
         return f"<User {self.email}>"
