821acd92e5bb30abcc4c5893f8171274713caafb
thanuavi1 <thanuavi1@gmail.com>
2021-11-16 22:10:51 +0000

    errors and bugs fixing

--- routes.py
+++ routes.py
@@ -186,4 +186,7 @@ def index():
 
 app.register_blueprint(bp)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8081)), debug=True
+    )
