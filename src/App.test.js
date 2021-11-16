1492c907226d0b5d534b450f695084e3447b5a53
Nora Parker <nparker19@student.gsu.edu>
2021-11-16 11:24:05 -0500

    Adjusted client side tests

--- src/App.test.js
+++ src/App.test.js
@@ -20,7 +20,7 @@ test('add event', () => {
   fireEvent.change(eventInput, { target: { value: 'Analysis II' } });
   fireEvent.click(addButton);
 
-  const eventEntry = screen.getByText('Analysis II from  to  ');
+  const eventEntry = screen.getByText('/Analysis II/');
   const deleteButton = screen.getByText('X');
   expect(eventEntry).toBeInTheDocument();
   expect(deleteButton).toBeInTheDocument();
@@ -36,7 +36,7 @@ test('delete event', () => {
   fireEvent.change(eventInput, { target: { value: 'Analysis II' } });
   fireEvent.click(addButton);
 
-  const eventEntry = screen.getByText('Analysis II from  to  ');
+  const eventEntry = screen.getByText('/Analysis II/');
   const deleteButton = screen.getByText('X');
   expect(eventEntry).toBeInTheDocument();
   expect(deleteButton).toBeInTheDocument();
