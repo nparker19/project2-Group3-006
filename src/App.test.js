d31feae4f3a081b0c54e66deca8b623456068f25
Nora Parker <nparker19@student.gsu.edu>
2021-11-16 10:52:30 -0500

    Adjusted client side tests

--- src/App.test.js
+++ src/App.test.js
@@ -19,18 +19,14 @@ test('add event', () => {
   const endInput = screen.getByTestId('end_input');
   const startInput = screen.getByTestId('start_input');
 
-  fireEvent.change(eventInput, { target: { value: 'Workout' } });
-  fireEvent.change(startInput, { target: { value: '10:00 AM' } });
-  fireEvent.change(endInput, { target: { value: '11:00 AM' } });
+  fireEvent.change(eventInput, { target: { value: 'Analysis II' } });
+  fireEvent.change(startInput, { target: { value: '03:45 PM' } });
+  fireEvent.change(endInput, { target: { value: '05:00 PM' } });
   fireEvent.click(addButton);
 
-  const eventEntry = screen.getByText('Workout');
-  const startEntry = screen.getByText('10:00');
-  const endEntry = screen.getByText('11:00');
+  const eventEntry = screen.getByText('Analysis II from 15:45 to 17:00 ');
   const deleteButton = screen.getByText('X');
   expect(eventEntry).toBeInTheDocument();
-  expect(startEntry).toBeInTheDocument();
-  expect(endEntry).toBeInTheDocument();
   expect(deleteButton).toBeInTheDocument();
 });
 
@@ -40,18 +36,16 @@ test('delete event', () => {
   const eventInput = screen.getByTestId('event_input');
   const endInput = screen.getByTestId('end_input');
   const startInput = screen.getByTestId('start_input');
-  fireEvent.change(eventInput, { target: { value: 'Workout' } });
-  fireEvent.change(startInput, { target: { value: '10:00 AM' } });
-  fireEvent.change(endInput, { target: { value: '11:00 AM' } });
+
+
+  fireEvent.change(eventInput, { target: { value: 'Analysis II' } });
+  fireEvent.change(startInput, { target: { value: '03:45 PM' } });
+  fireEvent.change(endInput, { target: { value: '05:00 PM' } });
   fireEvent.click(addButton);
 
-  const eventEntry = screen.getByText('Workout');
-  const startEntry = screen.getByText('10:00');
-  const endEntry = screen.getByText('11:00');
+  const eventEntry = screen.getByText('Analysis II from 15:45 to 17:00 ');
   const deleteButton = screen.getByText('X');
   expect(eventEntry).toBeInTheDocument();
-  expect(startEntry).toBeInTheDocument();
-  expect(endEntry).toBeInTheDocument();
   expect(deleteButton).toBeInTheDocument();
 
   fireEvent.click(deleteButton);
