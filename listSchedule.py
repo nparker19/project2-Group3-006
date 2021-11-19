9adb6b397edf84f6e5ad8fd0912962b5057d4c49
Your Name <tunabello@yahoo.com>
2021-11-19 08:59:34 +0000

    updating my branch with main pull

--- listSchedule.py
+++ listSchedule.py
@@ -12,7 +12,7 @@ def listSchedules():
     now = datetime.datetime.utcnow().isoformat() + 'Z'
     print('Getting List of 5 shedules')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=5, singleEvents=True,
+                                        maxResults=1, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
     
@@ -22,4 +22,5 @@ def listSchedules():
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
         print(listSchedules())
+        print(events)
 listSchedules()
