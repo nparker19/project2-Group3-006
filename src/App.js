76730b845367dd362dcc8855178d846e49fc6445
thanuavi1 <thanuavi1@gmail.com>
2021-11-20 04:58:42 +0000

    First push/agile story for Sprint 2 - Landing Page CSS and Functionality Content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import React, { useState, useRef } from 'react';
-
+import "./App.css";
+import React, { useState, useRef } from "react";
 
 function App() {
-
   const [scheduleDict, setScheduleDict] = useState([]);
-  const eventInput = useRef('');
-  const startTimeInput = useRef('');
-  const endTimeInput = useRef('');
-  const dateInput = useRef('');
-  const messages = useRef('');
+  const eventInput = useRef("");
+  const startTimeInput = useRef("");
+  const endTimeInput = useRef("");
+  const dateInput = useRef("");
+  const messages = useRef("");
 
   function Schedule(props) {
-
     function onDelete() {
       const newDict = scheduleDict.filter((item) => item.event !== props.item);
       setScheduleDict(newDict);
     }
     return (
-      <h2>{props.item} from {props.startTime} to {props.endTime}<button onClick={onDelete}>X</button></h2>
+      <h2>
+        {props.item} from {props.startTime} to {props.endTime}
+        <button onClick={onDelete}>X</button>
+      </h2>
     );
   }
 
@@ -26,7 +26,10 @@ function App() {
     let newEvent = eventInput.current.value;
     let newStartTime = startTimeInput.current.value;
     let newEndTime = endTimeInput.current.value;
-    let newScheduleDict = [...scheduleDict, { "event": newEvent, "startTime": newStartTime, "endTime": newEndTime }];
+    let newScheduleDict = [
+      ...scheduleDict,
+      { event: newEvent, startTime: newStartTime, endTime: newEndTime },
+    ];
 
     setScheduleDict(newScheduleDict);
 
@@ -36,34 +39,39 @@ function App() {
   }
 
   function onSaveClick() {
-
-    fetch('/suggestions', {
-      method: 'POST',
+    fetch("/suggestions", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ "scheduleDict": scheduleDict, "messages": messages }),
-    }).then(response => response.json()).then(data => {
-      for (let i = 0; i < data.message_server.length; i++) {
-        alert(data.message_server[i]);
-      }
-      setScheduleDict(data.schedule_server);
-    });
+      body: JSON.stringify({ scheduleDict: scheduleDict, messages: messages }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 0; i < data.message_server.length; i++) {
+          alert(data.message_server[i]);
+        }
+        setScheduleDict(data.schedule_server);
+      });
   }
 
   function onCompleteClick() {
-    const response_data = JSON.stringify({ "scheduleDict": scheduleDict, "currentDate": dateInput.current.value });
-    fetch('/complete', {
-      method: 'POST',
+    const response_data = JSON.stringify({
+      scheduleDict: scheduleDict,
+      currentDate: dateInput.current.value,
+    });
+    fetch("/complete", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: response_data,
-    }).then(response => response.json()).then(data => {
-      setScheduleDict(data.schedule_server);
-      window.location.replace("/");
-    });
-
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setScheduleDict(data.schedule_server);
+        window.location.replace("/");
+      });
   }
 
   return (
@@ -73,7 +81,11 @@ function App() {
       <input ref={dateInput} type="date" />
 
       <div class="scheduleList" align="center">
-        <h2>{scheduleDict.map((dictItem) => <Schedule item={dictItem.event} startTime={dictItem.startTime} endTime={dictItem.endTime} />)}</h2>
+        <h2>
+          {scheduleDict.map((dictItem) => (
+            <Schedule item={dictItem.event} startTime={dictItem.startTime} endTime={dictItem.endTime} />
+          ))}
+        </h2>
       </div>
 
       <div class="editSchedule" align="center">
@@ -91,11 +103,8 @@ function App() {
         <div class="Complete" align="center">
           <button onClick={() => onCompleteClick()}> Complete Schedule and save to google calendar</button>
         </div>
-
       </div>
     </div>
-
-
   );
 }
 
