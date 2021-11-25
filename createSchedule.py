22216257807a1a6ea82e0e507bd5d3822f99df27
Your Name <tunabello@yahoo.com>
2021-11-25 11:47:11 +0000

    modify listSchedule, route, home html

--- createSchedule.py
+++ createSchedule.py
@@ -19,11 +19,11 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def creatSchedules(x, militaryTime):
     service = Create_Service(
-        GOOGLE_APPLICATION_CREDENTIALS, API_NAME, APIVERSION, SCOPES
+        GOOGLE_APPLICATION_CREDENTIALS, API_NAME, API_VERSION, SCOPES
     )
     today = datetime.now().date()
     for i in x:
-        dict = i
+        dict_ = i
         dict = dict_
         event = dict["event"]
         startTime = dict["startTime"]
