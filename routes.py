d46ab20442a2c474b753fad23733d15f40114ead
Your Name <tunabello@yahoo.com>
2021-12-02 01:26:43 +0000

    update changes in routes

--- routes.py
+++ routes.py
@@ -297,7 +297,7 @@ app.register_blueprint(bp)
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8080")),
+        # host=os.getenv("IP", "0.0.0.0"),
+        # port=int(os.getenv("PORT", "8080")),
         debug=True,
     )
